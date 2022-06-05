bce370a75323698ffebdcd15c17c16a3e94fecc0
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2022-06-06 01:35:08 +0800

    Update
    airsim_basic_function.py
    drone_movement.py
    main.py
    read_file.py

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
 import airsim_basic_function as abf
 import control_algorithm as ca
-import drone_movement
-import read_file
+# import drone_movement
+import read_file as rf
 import subprocess
 import sys
 import os
 
 
-# Run another python script and close it after it's finished
 def spawn_program_and_die(program, exit_code=0):
+    """
+    Description:
+        Run another python script and close it after it's finished
+    Parameter:
+        Input:
+            program: (str) Execute program.
+        Output:
+            None
+    Link:
+        https://stackoverflow.com/questions/7974849/how-can-i-make-one-python-file-run-another
+    """
     subprocess.Popen(program)
     sys.exit(exit_code)
 
+
 if __name__ == '__main__':
+    """
+    Description:
+        Main function.
+    Parameter:
+        Input:
+            None
+        Output:
+            None
+    Link:
+        None
+    """
     # Read Coordinate
-    print('reading coordinate...')
-    spawn_program_and_die(['python', 'read_file.py'])
+    print('main.py: reading coordinate...')
+    coordinate = rf.read_csv('./test_coordinate/target_coordinate.csv')
+    print('main.py: coordinate read')
+    print(coordinate)
 
     # Initialize
-    print('initializing...')
+    print('main.py: initializing...')
     abf.drone_initialize()
+    print('main.py: initialized')
 
     # Take single piece of photo
-    print('taking picture...')
-    abf.capture_single_picture()
+    print('main.py: taking picture...')
+    abf.capture_single_picture('./captured_image', '1')
+    print('main.py: picture taken')
 
     # Perform drone movements
-    #print('moving drone...')
+    # print('moving drone...')
     # spawn_program_and_die(['python', 'drone_movement.py'])
+
+"""
+Description:
+    
+parameter:
+    Input:
+        
+    Output:
+        
+Link:
+    
+"""
--- read_file.py
+++ read_file.py
@@ -1,13 +1,27 @@
 import csv
 
 
-file = open('./test_coordinate/target_coordinate.csv', 'r')
-print('csv file opened')
-csvreader = csv.reader(file)
-print('csv file read')
-rows = []
-for row in csvreader:
-    rows.append(row)
-print('coordinate data appended')
-print(rows)
-file.close()
+def read_csv(program):
+    """
+    Description:
+        Read data from input file.
+    Parameter:
+        Input:
+            program: (str) File directory and file name of target csv file.
+        Output:
+            return: (str/arr) Array of read data. (If the file include header, it will be included)
+    Link:
+        https://stackoverflow.com/questions/56870272/how-to-execute-another-python-file-and-then-close-the-existing-one/56871072#56871072
+        https://realpython.com/python-return-statement/
+    """
+    file = open(program)
+    print('read_file.py: csv file opened')
+    csvreader = csv.reader(file)
+    print('read_file.py: csv file read')
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+    print('read_file.py: coordinate data appended')
+    #print(rows)
+    file.close()
+    return rows
