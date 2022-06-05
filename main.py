98f1657975a41e8bdcc6ffdf184c83eddefc1fbc
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2022-06-05 23:15:30 +0800

    update
    main.py
    read_file.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import airsim_basic_function as abf
 import control_algorithm as ca
 import drone_movement
-import read_file as rf
+import read_file
 import subprocess
 import sys
 import os
 
 
+# Run another python script and close it after it's finished
 def spawn_program_and_die(program, exit_code=0):
     subprocess.Popen(program)
     sys.exit(exit_code)
 
+if __name__ == '__main__':
+    # Read Coordinate
+    print('reading coordinate...')
+    spawn_program_and_die(['python', 'read_file.py'])
 
-# Initialize
-abf.drone_initialize()
+    # Initialize
+    print('initializing...')
+    abf.drone_initialize()
 
-# Take single piece of photo
-abf.capture_single_picture()
+    # Take single piece of photo
+    print('taking picture...')
+    abf.capture_single_picture()
 
-# Perform drone movements
-spawn_program_and_die(['python', 'drone_movement.py'])
+    # Perform drone movements
+    #print('moving drone...')
+    # spawn_program_and_die(['python', 'drone_movement.py'])
--- read_file.py
+++ read_file.py
@@ -1,2 +1,13 @@
 import csv
 
+
+file = open('./test_coordinate/target_coordinate.csv', 'r')
+print('csv file opened')
+csvreader = csv.reader(file)
+print('csv file read')
+rows = []
+for row in csvreader:
+    rows.append(row)
+print('coordinate data appended')
+print(rows)
+file.close()
