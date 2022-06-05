63f812adcb63b33a45295b106386be9326c90c7a
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2022-06-06 02:16:32 +0800

    Update
    read_file.py

--- read_file.py
+++ read_file.py
@@ -9,7 +9,7 @@ def read_csv(program):
         Input:
             program: (str) File directory and file name of target csv file.
         Output:
-            return: (str/arr) Array of read data. (If the file include header, it will be included)
+            return: (str/2nd.arr) Array of read data. (If the file include header, it will be included)
     Link:
         https://stackoverflow.com/questions/56870272/how-to-execute-another-python-file-and-then-close-the-existing-one/56871072#56871072
         https://realpython.com/python-return-statement/
@@ -30,12 +30,13 @@ def read_csv(program):
 def print_csv_data(array, row_number):
     """
     Description:
-
+        Print out all the elements of single row from the target 2 order array.
     parameter:
         Input:
-
+            array: (str/2nd.arr) Target array name.
+            row_number: (int) Target row number.
         Output:
-
+            None
     Link:
         https://stackoverflow.com/questions/16548668/iterating-over-a-2-dimensional-python-list
     """
