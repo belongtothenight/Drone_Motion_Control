f93226245cc56f954528626aa99fd16f2aa7e95e
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2022-06-06 09:08:35 +0800

    Update drone_movement.py

--- drone_movement.py
+++ drone_movement.py
@@ -7,12 +7,7 @@ import read_file as rf
 print('drone_movement.py-> reading coordinate...')
 coordinate = rf.read_csv('./test_coordinate/target_coordinate.csv')
 cor, t_row, t_col = rf.get_csv_data_single_row_adj_element(coordinate, 0, 2, 11)
-'''
-    print_csv_data_single_row_adj_element() function test
-        print('main.py-> coordinate array: ', cor)
-        print('main.py-> total row: ', t_row)
-        print('main.py-> total column: ', t_col)
-    '''
+
 
 # Take off
 client = airsim.MultirotorClient()
@@ -20,8 +15,17 @@ client.takeoffAsync().join()
 print('drone_movement.py-> Drone take off')
 
 # Approach filming location
+'''
+Use center coordinate to adjust detail relative coordinate.
+Use the distance times a value as actual direct 
+distance, and use known altitude, to calculate x and y movement to reach filming coordinate. 
+'''
 
 # Approach target
+'''
+Use data above to locate drone at the top center of car.
+'''
+
 
 # Landing
 
